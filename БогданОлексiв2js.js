baf669ff0d65ad7b7231d77fce99ef79e0956705
klipJamper <61622353+klipJamper@users.noreply.github.com>
2020-02-29 14:50:32 +0200

    Add files via upload

---
+++ БогданОлексiв2js.js
@@ -0,0 +1,31 @@
+let name = prompt("яке ваше ім'я:");
+let surname = prompt("яке ваше прізвище:");
+let age = prompt('ваш логін:');
+let w = prompt('ваш eмеіл:');
+let a = prompt("ваш пароль(цифри):");
+let b = prompt("повторіть пароль:");
+let q = prompt("яка ваша країна:")
+    alert('Доброго дня' + " " + name + " " + surname + " :)")
+    if (a == b) {
+        alert('пароль співпадає!');
+    } else {
+        alert('пароль не співпадає!');
+        let a = prompt("ваш пароль:");
+        let b = prompt("повторіть пароль:");
+    }
+    if (a == b) {
+        alert('пароль співпадає!');
+    } else {
+        alert('пароль не співпадає!');
+    }
+        if (w == w ) {
+            alert("Ваш акаунт створено :) ");
+        } else {
+            alert('не правильний eмеіл!');
+            let w = prompt('ваш eмеіл:');
+        }
+        if (w == q) {
+            alert("Ваш акаунт створено :) ");
+        } else {
+            alert('не правильний eмеіл!');
+        }
